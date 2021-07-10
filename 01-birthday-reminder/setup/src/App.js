74d5cbd974e9c2939ab09663022b54cef33856fe
Ziad Bennadji <matder.dz@gmail.com>
2021-07-10 18:19:27 +0100

    solve the first project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [pepole, setPeople] = useState(data);
+  return <>
+  <main>
+  <div className="container">
+      <h3>{pepole.length +'birthdays today'}</h3>
+      <List pepole={pepole}/>
+      <button className="btn" onClick={()=>{setPeople([])}}>clear list</button>
+
+    </div>
+  </main>
+   
+  </>
 }
 
 export default App;
